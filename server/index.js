55fd129fb15439f518c9ca427fc0fcb538a1ca24
AmadeuszPodlejski <amadeusz.podlejski@int.pl>
2024-10-08 13:52:19 +0200

    Commit1

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
+const { UserModel} = require("./models/user");
 
 
 const cors = require('cors');
@@ -12,5 +13,42 @@ mongoose.connect("mongodb://localhost:27017/master_planer");
 
 
 app.listen(3001, () =>{
-  console.log("Server is running with nodemon");
+  console.log("server is running with nodemon");
+});
+
+app.get("/getUsers", async (req, res) =>{
+  UserModel.find({})
+    .then(result => {
+      res.json(result);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+app.post("/createUser", async (req, res) => {
+  const obj = req.body;
+  const newUser = new UserModel(obj);
+  await newUser.save();
+  res.json(obj);
+});
+
+app.post("/createEvent", async (req, res) => {
+  const {_id, event} = req.body;
+  try{
+    const updatedUser = await UserModel.findByIdAndUpdate(
+    _id,
+    {$push: { events: event }},
+    {new: true, useFindAndModify: false}
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found." });
+    }
+    res.json(updatedUser.events);
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error adding event.", error });
+  }
 });
---
+++ server/models/user.js
@@ -0,0 +1,67 @@
+const mongoose = require("mongoose");
+
+const eventSchema = new mongoose.Schema({
+  name: { 
+    type: String, 
+    required: true 
+  },
+  desc: { 
+    type: String,
+    default: ""
+  },
+  color: { 
+    type: String, 
+    required: true,
+    default: "Blue"
+  }
+});
+
+const datedEventSchema = new mongoose.Schema({
+  name: { 
+    type: String, 
+    required: true 
+  },
+  desc: { 
+    type: String,
+    default: "" 
+  },
+  color: { 
+    type: String,
+    required: true,
+    default: "Blue" 
+  },
+  date: {
+    type: [Date],
+    required: true
+  }
+});
+
+const userSchema = new mongoose.Schema({
+  nick: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true 
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  events: {
+    type: [eventSchema],
+    default: Array
+  },
+  dated_events: {
+    type: [datedEventSchema],
+    default: Array
+  }
+}, {
+  versionKey: false // Disables the __v field
+});
+
+const UserModel = mongoose.model("users", userSchema);
+//  collection name in mongoDB    ^^^
+module.exports = { UserModel };
