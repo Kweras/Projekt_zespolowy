bd139d754bed1090115c1b8d993a7a0d70b1f1b0
Marcin Ptok <marcin111100@gmail.com>
2024-10-31 00:45:54 +0100

    Small fixes to hour indicator

--- client/src/components/calendar/WeekView.jsx
+++ client/src/components/calendar/WeekView.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { getScrollbarWidth } from '../../utils/getScrollbarWidth';
 import { getDaysOfTheWeek } from '../../utils/getDaysOfTheWeek';
 import { getWeekNumber } from '../../utils/getWeekNumber';
@@ -7,16 +7,17 @@ import { getPolishDayOfWeek } from '../../utils/getPolishWeekDays';
 
 export default function WeekView({ currentDate }) {
   const hoursContainerRef = useRef(null);
+  const dayColumnRef = useRef(null);
+  const [hourIndicator, setHourIndicator] = useState({}); 
+
   const scrollBarWidth = getScrollbarWidth();
   const gridColumnsWithoutScrollbar = `50px 10px 1fr 1fr 1fr 1fr 1fr 1fr 1fr ${scrollBarWidth}px`;
+
   const daysOfTheWeek = getDaysOfTheWeek(getWeekNumber(currentDate), currentDate.getFullYear());
 
   const daysElement = daysOfTheWeek.map(day => {
     return (<div className="day-container" key={day.getTime()}>{getPolishDayOfWeek(day)} <span>{day.getDate()}</span></div>)
   });
-
-  const now = new Date();
-  const nowElementTop = (now.getHours()) * 50 + (now.getMinutes() * (50 / 60));
 
   useEffect(() => {
     const now = new Date();
@@ -27,7 +28,22 @@ export default function WeekView({ currentDate }) {
         behavior: 'smooth',
       });
     }
-  }, []);
+
+    if (dayColumnRef) {
+      const day = now.getDay();
+      const weekDay = day === 0 ? 7 : day;
+      
+      const todayAtMidnight = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+      const isTodayInRange = daysOfTheWeek.filter(el => el.getTime() === todayAtMidnight.getTime());
+      
+      setHourIndicator(() => ({
+        top: (now.getHours()) * 50 + (now.getMinutes() * (50 / 60)),
+        width: dayColumnRef.current.offsetWidth,
+        left: dayColumnRef.current.offsetWidth * (weekDay - 1),
+        display: isTodayInRange.length > 0 ? 'block' : 'none'
+      }))
+    }
+  }, [currentDate]);
 
   return (
     <div className="week-view-container" >
@@ -80,39 +96,47 @@ export default function WeekView({ currentDate }) {
         <div className="dashes-container">
           <SpamOfDivs />
         </div>
-        <div className="day-events">
+
+
+        <div className="day-events" data-day="monday" ref={dayColumnRef}>
+          {<HourIndicator {...hourIndicator} />}
           <SpamOfDivs />
           <div></div>
         </div>
-        <div className="day-events">
+        <div className="day-events" data-day="tuesday">
           <SpamOfDivs />
           <div></div>
         </div>
-        <div className="day-events">
+        <div className="day-events" data-day="wednesday">
+          <div className="event" style={{ top: `${50 * 14}px`, height: `${50 * 4}px` }}>Projekt zespołowy</div>
           <SpamOfDivs />
           <div></div>
         </div>
-        <div className="day-events">
-          <div className="now-element" style={{top: `${nowElementTop}px`}}></div>
-          <div className="event" style={{ top: `${50 * 14}px`, height: `${50 * 4}px`}}>Projekt zespołowy</div>
+        <div className="day-events" data-day="thursday">
           <SpamOfDivs />
           <div></div>
         </div>
-        <div className="day-events">
+        <div className="day-events" data-day="friday">
           <SpamOfDivs />
           <div></div>
         </div>
-        <div className="day-events">
+        <div className="day-events" data-day="saturday">
           <div className='event'> test</div>
           <SpamOfDivs />
           <div></div>
         </div>
-        <div className="day-events">
+        <div className="day-events" data-day="sunday">
           <SpamOfDivs />
           <div></div>
         </div>
       </div>
     </div>
+  )
+}
+
+const HourIndicator = ({top, left, width, display}) => {  
+  return (
+    <div className="now-element" style={{ width: `${width}px`, top: `${top}px`, left: `${left}px`, display }}></div>
   )
 }
 
